a06da4d470c4bcd79132a02ebd42b1accf72a09c
SISIR SEN <sisirsen22122004@gmail.com>
2026-05-06 02:07:28 +0530

    Src folder with components, logos, icons

---
+++ src/components/footer.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import icons from "../utils/iconAccess";
+
+const Footer = () => {
+  return (
+    <footer className="bg-[#05091A] text-white px-6 py-6">
+     
+      <div className="flex justify-between w-full  ">
+       
+        <div>
+          <h1
+            className="bg-gradient-to-r from-blue-500 to-purple-500 
+               bg-clip-text text-transparent 
+               text-2xl font-bold 
+               w-fit "
+          >
+            SISIR SEN
+          </h1>
+          <p className="text-sm mt-2 text-gray-400">
+            Build with modern web experiences.
+          </p>
+        </div>
+
+        <div className="flex items-center text-sky-500">   
+          <div className="flex gap-5">
+            <button className="cursor-pointer h-6 w-6"><img src={icons.gitButton} alt="githubButton" /></button>
+            <button className="cursor-pointer h-5 w-4"><img src={icons.linkedinButton} alt="linkedInButton" /></button>
+            <button className="cursor-pointer h-7 w-7"><img src={icons.xButton} alt="XButton" /></button>
+          </div>
+        </div>
+      </div>
+
+
+      <div className="border-t border-gray-700 mt-8 pt-4 text-center text-sm text-gray-400">
+        © {new Date().getFullYear()} Sisir Sen. All rights reserved.
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/components/navbar.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import icons from '../utils/iconAccess'
+
+function Navbar() {
+  const navItems=[
+    {item:'Home'},
+    {item:'About'},
+    {item:'Skills'},
+    {item:'Projects'},
+    {item:'Contact'}
+  ]
+  return (
+    <div className='h-screen bg-[#0F172A]'>
+      <div className='flex p-4 items-center bg-[#05091A] '>
+        <div className=' bg-gradient-to-r from-blue-500 to-purple-500 bg-clip-text text-transparent text-2xl font-bold'>SISIR SEN</div>
+
+        <div className='md:hidden h-7 w-7 ml-auto '><img src={icons.menuButton}  alt="menu" /></div>
+
+        <div className='hidden sm:flex  ml-auto gap-8 text-lg text-white cursor-pointer'>
+        {navItems.map((items)=> <p className='hover:underline'>{items.item}</p>
+        )}
+      </div>
+      </div>
+
+      
+    </div>
+  )
+}
+
+export default Navbar
